ea7395be1d42733a2eda24fcf00d879c96b0e277
jmaboagye <67410347+jmaboagye@users.noreply.github.com>
2020-08-07 19:43:53 -0500

    Add files via upload
    adding bookmarker and quiz project

---
+++ quizapp/question.js
@@ -0,0 +1,45 @@
+
+var questions = [{
+    "question": "Inside which HTML element do we put the JavaScript?", 
+	"option1": "<script>",
+  "option2": "<head>",
+  "option3": "<meta>",
+  "option4": "<style>",
+  "answer": "1"
+  }, {
+  "question": "Which of the following is correct about features of JavaScript?",
+  "option1":  "It can not Handling dates and time",
+  "option2":  "JavaScript is a object-based scripting language",
+  "option3":  "JavaScript is not interpreter based scripting language",
+  "option4":  "All of the above"
+  "answer": "2"
+   }, {
+   "question": "Which of the following is the correct syntax to display “Letsfindcourse” in an alert box using JavaScript?",
+   "option1": "alert-box(“Letsfindcourse”)",
+    "option2": "confirm("Letsfindcourse")",
+   "option3": "msgbox(“Letsfindcourse”)",
+   "option4": "alert(“Letsfindcourse”)",
+   "answer": "4"
+   }, {
+     "question": "Which symbol is used for comments in Javascript?",
+     "option1":  "\\",
+     "option2":  "//",
+     "option3":  "\* *\ ",
+     "option4":  "\* */",
+     "answer": "2"
+   }, { 
+   "question": "Which of the following method checks if its argument is not a number?",
+   "option1": "isNaN()",
+   "option2": "nonNaN()",
+   "option3": "NaN()",
+   "option4": "None of the above",
+   "answer": "1"
+   }, { 
+   "question": "JavaScript can be written __________",
+   "option1": "directly into JS file and included into HTML",
+   "otion2": "directly on the server page",
+   "option3": "directly into HTML pages",
+   "option4": "directly into the css file",
+   "answer": "1"
+
+}];
